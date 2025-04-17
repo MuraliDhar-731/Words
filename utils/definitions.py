147b79ad061f2a792965fed6909b8fe6fd9492f9
MuraliDhar-731 <mkoli3@unh.newhaven.edu>
2025-04-17 13:02:04 -0400

    Update definitions.py

--- utils/definitions.py
+++ utils/definitions.py
@@ -1,1 +1,15 @@
-# Word definition functions (placeholder)
+from PyDictionary import PyDictionary
+
+dictionary = PyDictionary()
+
+def get_word_info(word):
+    try:
+        meaning = dictionary.meaning(word)
+        example = dictionary.get_examples(word)
+        if meaning:
+            meaning_text = list(meaning.values())[0][0]
+        else:
+            meaning_text = "Not found"
+        return meaning_text, example
+    except:
+        return "Not found", []
